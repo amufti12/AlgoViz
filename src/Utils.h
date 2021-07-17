0cba17ca4fce82cdbdf0fe9acf39173c350d700a
amufti12 <58714536+amufti12@users.noreply.github.com>
2021-07-17 17:46:33 -0400

    Implemented shaders and vizualized random array

--- src/Utils.h
+++ src/Utils.h
@@ -1,9 +1,37 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <glad/glad.h>
+
 struct SortableRenderData
 {
-  float data;
+public:
+  SortableRenderData()
+  {
+    // Initializing VAO, (initilize 1 vao then bind)
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Initializing VBO
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(float), vertexData, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Initializing EBO
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), indicies, GL_STATIC_DRAW);
+
+    // Unbinds currently bound vertex array
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  }
 
   bool operator<(const SortableRenderData& srd) { return data < srd.data; }
 
@@ -14,6 +42,38 @@ struct SortableRenderData
   bool operator>=(const SortableRenderData& srd) { return data >= srd.data; }
 
   friend std::ostream& operator<<(std::ostream& os, const SortableRenderData& srd);
+
+public:
+  float data;
+  // Vertex array object
+  uint32_t vao;
+  // Vertex buffer object
+  uint32_t vbo;
+  // Element buffer object
+  uint32_t ebo;
+
+  bool sorted;
+
+private:
+  inline static float vertexData[] = {
+    0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, //
+    0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, //
+    1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, //
+    1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f  //
+  };
+
+  // inline static float vertexData[] = {
+  //   -0.5f, 0.5f,  0.0f, 1.0f, 0.0f, 0.0f, //
+  //   0.5f,  0.5f,  0.0f, 1.0f, 1.0f, 0.0f, //
+  //   -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, //
+  //   0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 1.0f  //
+  // };
+
+  // Order to run the vertecies (both in winding order)
+  inline static uint32_t indicies[] = {
+    0, 1, 2, //
+    2, 1, 3  //
+  };
 };
 
 std::ostream& operator<<(std::ostream& os, const SortableRenderData& srd)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <algorithm>
+
 #include <imgui.h>
 
 #include <backends/imgui_impl_glfw.h>
@@ -8,7 +10,12 @@
 #include <glad/glad.h>
 
 #include <GLFW/glfw3.h>
+#pragma warning(push)
+#pragma warning(disable : 4201)
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#pragma warning(pop)
 
 #include "util/Logger.h"
 
@@ -30,62 +37,67 @@ int main(int argc, char** argv)
   AV_UNUSED(argc);
   AV_UNUSED(argv);
 
+  srand(static_cast<uint32_t>(time(NULL)));
+
   Logger::Init();
   Logger::SetLogLevel(Logger::LogLevel::INFO);
 
-  // Make array you want to test and call function from Sorts
-  SortableRenderData array[10];
-  GetArray(array, 10);
-  std::cout << "Test Array:\t";
-  for (int i = 0; i < 10; i++) {
-    std::cout << array[i] << " ";
-  }
-  std::cout << std::endl;
+  // ////////////////////////////////////////////////////////
+  // // SORTING ALGORITHM TEST SECTION
+  // // Make array you want to test and call function from Sorts
+  // SortableRenderData array[10];
+  // GetArray(array, 10);
+  // std::cout << "Test Array:\t";
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << array[i] << " ";
+  // }
+  // std::cout << std::endl;
 
-  // Selection Sort Test
-  selectionSort(array, 10);
-  std::cout << "Selection Sort:\t";
-  for (int i = 0; i < 10; i++) {
-    std::cout << array[i] << " ";
-  }
-  std::cout << std::endl;
+  // // Selection Sort Test
+  // selectionSort(array, 10);
+  // std::cout << "Selection Sort:\t";
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << array[i] << " ";
+  // }
+  // std::cout << std::endl;
 
-  // Bubble Sort Test
-  GetArray(array, 10);
-  bubbleSort(array, 10);
-  std::cout << "Bubble Sort:\t";
-  for (int i = 0; i < 10; i++) {
-    std::cout << array[i] << " ";
-  }
-  std::cout << std::endl;
+  // // Bubble Sort Test
+  // GetArray(array, 10);
+  // bubbleSort(array, 10);
+  // std::cout << "Bubble Sort:\t";
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << array[i] << " ";
+  // }
+  // std::cout << std::endl;
 
-  // Insertion Sort Test
-  GetArray(array, 10);
-  insertionSort(array, 10);
-  std::cout << "Insertion Sort:\t";
-  for (int i = 0; i < 10; i++) {
-    std::cout << array[i] << " ";
-  }
-  std::cout << std::endl;
+  // // Insertion Sort Test
+  // GetArray(array, 10);
+  // insertionSort(array, 10);
+  // std::cout << "Insertion Sort:\t";
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << array[i] << " ";
+  // }
+  // std::cout << std::endl;
 
-  // Merge Sort Test
-  GetArray(array, 10);
-  mergeSort(array, 0, 9);
-  std::cout << "Merge Sort:\t";
-  for (int i = 0; i < 10; i++) {
-    std::cout << array[i] << " ";
-  }
-  std::cout << std::endl;
+  // // Merge Sort Test
+  // GetArray(array, 10);
+  // mergeSort(array, 0, 9);
+  // std::cout << "Merge Sort:\t";
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << array[i] << " ";
+  // }
+  // std::cout << std::endl;
 
-  // Quick Sort Test
-  GetArray(array, 10);
-  mergeSort(array, 0, 9);
-  std::cout << "Quick Sort:\t";
-  for (int i = 0; i < 10; i++) {
-    std::cout << array[i] << " ";
-  }
-  std::cout << std::endl;
+  // // Quick Sort Test
+  // GetArray(array, 10);
+  // mergeSort(array, 0, 9);
+  // std::cout << "Quick Sort:\t";
+  // for (int i = 0; i < 10; i++) {
+  //   std::cout << array[i] << " ";
+  // }
+  // std::cout << std::endl;
   ////////////////////////////////////////////////////////
+  // CREATING GLFW WINDOW SECTION
   if (glfwInit() != GLFW_TRUE) {
     LOG_ERROR("GLFW failed to initialize {}", 10);
     std::exit(-1);
@@ -102,24 +114,101 @@ int main(int argc, char** argv)
   GLFWwindow* window = glfwCreateWindow(1280, 720, "AlgoViz", nullptr, nullptr);
   glfwMakeContextCurrent(window);
 
+  // Loading all function pointers for OpenGL
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     LOG_ERROR("Failed to initialize GLAD");
     std::exit(-1);
   }
 
-  // IMGUI_CHECKVERSION();
-  // ImGui::CreateContext();
-  // ImGuiIO& io = ImGui::GetIO();
-  // AV_UNUSED(io);
+  ////////////////////////////////////////////////////////
+  // SHADER SECTION
+  // Setting up Shader
+  // --Vertex shader source
+  const char* vertSource = "#version 460 core\n"
+                           "layout (location = 0) in vec3 iPos;\n"
+                           "layout (location = 1) in vec3 iMystery;\n"
+                           "uniform mat4 model;\n"
+                           "uniform mat4 projection;\n"
+                           "\n"
+                           "out vec3 oMystery;"
+                           "void main() {\n"
+                           "  gl_Position = projection * model * vec4(iPos, 1.0);\n"
+                           "  oMystery = iMystery;\n"
+                           "}\n";
 
-  // ImGui::StyleColorsDark();
+  // --Fragment Shader source (outputs color)
+  const char* fragSource = "#version 460 core\n"
+                           "in vec3 oMystery;\n"
+                           "out vec4 fragColor;\n"
+                           "void main(){\n"
+                           "  fragColor = vec4(oMystery, 1.0);\n"
+                           "}\n";
 
-  // ImGui_ImplGlfw_InitForOpenGL(window, true);
-  // ImGui_ImplOpenGL3_Init("#version 460");
+  // Compiling the Shader
+  uint32_t vertexShader, fragmentShader;
+  int32_t success;
+  char infoLog[512];
+
+  // Creating the vertexShader
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertSource, nullptr);
+  glCompileShader(vertexShader);
+
+  // Getting vertexShader compile status and reporting
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+    LOG_ERROR("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{}", infoLog);
+    std::exit(-1);
+  }
+
+  // Creating the fragment shader
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragSource, nullptr);
+  glCompileShader(fragmentShader);
+
+  // Getting fragementShader compile status and reporting
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+    LOG_ERROR("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n{}", infoLog);
+    std::exit(-1);
+  }
+
+  // Making a Shader Program
+  uint32_t program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+
+  // Getting program link status and reporting
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(program, 512, nullptr, infoLog);
+    LOG_ERROR("ERROR::SHADER::PROGRAM::LINKING_FAILED\n{}", infoLog);
+    std::exit(-1);
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  ////////////////////////////////////////////////////////
+
+  glViewport(0, 0, 1280, 720);
 
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-  // bool show = true;
+  SortableRenderData array[100];
+  GetArray(array, 100);
+  selectionSort(array, 100);
+  std::cout << "Selection Sort:\t";
+  for (int i = 0; i < 100; i++) {
+    std::cout << array[i] << " ";
+  }
+  std::cout << std::endl;
+
+  glm::vec2 position(0.0f, 0.0f);
+  float diff = 1280.0f / 100.0f;
+
   while (!glfwWindowShouldClose(window)) {
     // Get user input
     glfwPollEvents();
@@ -127,22 +216,27 @@ int main(int argc, char** argv)
     // Render
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // ImGui_ImplOpenGL3_NewFrame();
-    // ImGui_ImplGlfw_NewFrame();
-    // ImGui::NewFrame();
+    for (int i = 0; i < 100; i++) {
+      SortableRenderData srd = array[i];
+      glm::mat4 projection = glm::ortho(0.0f, 1280.0f, 720.0f, 0.0f);
+      glm::mat4 model = glm::mat4(1);
+      model = glm::translate(model, glm::vec3(position.x, position.y, 0.0f));
+      model = glm::scale(model, glm::vec3(1280.0f / 100.0f, srd.data * 720.0f, 1.0f));
 
-    // ImGui::ShowDemoWindow(&show);
-
-    // ImGui::Render();
-    // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+      glUseProgram(program);
+      glUniformMatrix4fv(
+        glGetUniformLocation(program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+      glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+      glBindVertexArray(srd.vao);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      glBindVertexArray(0);
+      position.x += diff;
+    }
+    position = glm::vec3(0.0f, 0.0f, 0.0f);
 
     // Put the render on the screen
     glfwSwapBuffers(window);
   }
-
-  // ImGui_ImplOpenGL3_Shutdown();
-  // ImGui_ImplGlfw_Shutdown();
-  // ImGui::DestroyContext();
 
   glfwTerminate();
 }
